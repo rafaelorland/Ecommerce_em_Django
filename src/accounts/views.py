848a3a34b0abeca0b859e79385075fbcf0344e1e
rafael orlando <rafael.orlando2023@gmail.com>
2023-06-12 11:07:09 -0300

    commit 11:07

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -34,9 +34,9 @@ def login_page(request):
     next_post = request.POST.get('next')
     redirect_path = next_ or next_post or None
     if form.is_valid():
-        username = form.cleaned_data.get("username")
-        password = form.cleaned_data.get("password")
-        user = authenticate(request, username=username, password=password) 
+        nome = form.cleaned_data.get("username")
+        senha = form.cleaned_data.get("password")
+        user = authenticate(request, username=nome, password=senha) 
         if user is not None:
             login(request, user)
             try:
